3782d9664bee6a0304eb2847cfc614856107efd0
happyme7245 <162317908+happyme7245@users.noreply.github.com>
2024-08-26 14:00:18 +0900

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,12 @@
+'''
+pos -> 10,000
+neg -> 50,000
+x duplicated sequences
+equal number of real pos = generated seq
+
+SPC donor
+SPC acceptor
+GWH chr1 donor
+GWH chr1 acceptor
+do all these trained
+'''
